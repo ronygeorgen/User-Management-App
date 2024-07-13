ec31b3aaeebacf2e8fd672a2bbf1bb31e742e471
Rony George <ronygeorgengit@gmail.com>
2024-07-13 12:05:35 +0530

    Backend folder created

--- Backend/UserManagementApp/user_app/models.py
+++ Backend/UserManagementApp/user_app/models.py
@@ -57,3 +57,9 @@ class CustomUser(AbstractBaseUser):
 
     def __str__(self):
         return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+    
+    def has_module_perms(self,app_label):
+        return True
